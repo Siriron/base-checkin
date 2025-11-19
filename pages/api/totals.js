98770246650f16827d1396238b3d7acc1059ee8e
Siriron <127705750+Siriron@users.noreply.github.com>
2025-11-19 11:29:19 +0600

    Update and rename totals.ts to totals.js

--- pages/api/totals.js
+++ pages/api/totals.js
@@ -1,8 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+const totals = {};
 
-const totals: Record<string, number> = {};
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req, res) {
   if (req.method === "GET") {
     res.status(200).json(totals);
   } else if (req.method === "POST") {
