41d404451d886fed882635d841243429c8b437cf
Siriron <127705750+Siriron@users.noreply.github.com>
2025-12-11 10:34:31 +0600

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="base:app_id"
+          content="693a4815e6be54f5ed71d5a6"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
